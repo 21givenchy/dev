d13b6902e8191aac9b27e3d2dcb3d6008e66a24b
21givenchy <65071041+21givenchy@users.noreply.github.com>
2023-07-16 05:25:18 +0000

    setup

---
+++ twitter/src/App.jsx
@@ -0,0 +1,13 @@
+ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Feed from './pages/Feed'
+function App() {
+  return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
